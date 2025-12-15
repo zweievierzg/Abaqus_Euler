955160bf48b52c61dc9ebd4de5f959047dfdc8f5
Zweievierzg <wilang@ethz.ch>
2025-12-15 09:42:54 +0100

    commented all abaqus imports

--- main.py
+++ main.py
@@ -7,23 +7,23 @@ import numpy as np
 
 
 
-import section
-import regionToolset
-import displayGroupMdbToolset as dgm
-import part
-import material
-import assembly
-import step
-import interaction
-import load
-import mesh
-import optimization
-import job
-import sketch
-import visualization
-import xyPlot
-import displayGroupOdbToolset as dgo
-import connectorBehavior
+# import section
+# import regionToolset
+# import displayGroupMdbToolset as dgm
+# import part
+# import material
+# import assembly
+# import step
+# import interaction
+# import load
+# import mesh
+# import optimization
+# import job
+# import sketch
+# import visualization
+# import xyPlot
+# import displayGroupOdbToolset as dgo
+# import connectorBehavior
 
 import os
 import sys
